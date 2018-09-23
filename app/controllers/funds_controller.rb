e3f151b28192c7306d329862e86ccbc508dc11b0
yutingcxiang <yutingcxiang@gmail.com>
2018-09-22 23:39:48 -0400

    Add routes and params for portfoliocontroller

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,2 +1,22 @@
 class FundsController < ApplicationController
+  def index
+    @funds = Fund.all
+  end
+
+  def new
+    @fund = Fund.new
+  end
+
+  def create
+  end
+
+  def show
+    @fund = Fund.find_by(id: params[:id])
+  end
+
+  private
+
+  def fund_params
+    params.require(:fund).permit(:name, :quantity, :price, :type)
+  end
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,33 @@
 class PortfoliosController < ApplicationController
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    #generate form
+  end
+
+  def show
+    @portfolio = Portfolio.find_by(id: params[:id])
+  end
+
+  def edit
+    @portfolio = Portfolio.find_by(id: params[:id])
+  end
+
+  def update
+    #create form
+  end
+
+  def destroy
+    @portfolio = Portfolio.find_by(id: params[:id])
+    @Portfolio.destroy
+    redirect_to '/'
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:name, :strategy, :email)
+  end
 end
