730af9d2eed126015476600f02c3b70820108cbb
yutingcxiang <yutingcxiang@gmail.com>
2018-09-23 01:22:13 -0400

    Fix broken signup button on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :funds, only: [:index, :new, :create, :show]
-  resources :portfolios
-  resources :users, only: [:index, :new, :create, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
+  resources :users, only: [:index, :new, :create, :show]
+  resources :portfolios
+  resources :funds, only: [:index, :new, :create, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
