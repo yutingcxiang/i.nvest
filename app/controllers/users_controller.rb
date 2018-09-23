8c1565b54fc1e686b164dfd7846f5add03f696b3
yutingcxiang <yutingcxiang@gmail.com>
2018-09-22 23:22:55 -0400

    Add routes and user_params to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
 end
