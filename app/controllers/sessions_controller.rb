c5373a2955434eaaad509d70ee0e1148f71c71a2
yutingcxiang <yutingcxiang@gmail.com>
2018-09-24 01:08:12 -0400

    Fix login and signup forms on homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      session[user_id] = @user
-      redirect_to users_path(@user)
+      session[:user_id] = @user
+      redirect_to user_path(@user)
     else
-      render 'users/new'
+      render new_user_path
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[user_id] = @user.id
-      redirect_to users_path(@user)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
-      render 'users/new'
+      render new_user_path
     end
   end
 
