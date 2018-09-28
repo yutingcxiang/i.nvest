0279929a2c05aed72da2855e90016031a393310e
yutingcxiang <yutingcxiang@gmail.com>
2018-09-28 18:38:14 -0400

    Add initial omniauth setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,34 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
+    if auth_hash
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.username = auth['info']['name']
+        u.email = auth['info']['email']
+        u.password = SecureRandom.hex
+      end
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
-      flash[:notice] = "Invalid username or password. Please try again."
-      redirect_to signin_path
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user.id)
+      else
+        flash[:notice] = "Invalid username or password. Please try again."
+        redirect_to signin_path
+      end
     end
   end
 
   def destroy
     session.clear
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   root 'application#home'
+
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
-  get '/signout' => 'sessions#destroy'
+  get '/signout' => 'sessions#destroy', as: 'signout'
+
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/auth/failure', to: redirect('/')
 
   resources :users, only: [:index, :new, :create, :show] do
     resources :funds
   end
 
-  resources :investments, only: [:index, :new, :create, :show, :destroy]
+  resources :investments, only: [:index, :new, :create, :show]
 end
