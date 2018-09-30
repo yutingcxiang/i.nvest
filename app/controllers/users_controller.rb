f847235a6faf69c9c39f667234887f0f53424a97
yutingcxiang <yutingcxiang@gmail.com>
2018-09-29 23:48:05 -0400

    Update edit form for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(username: params[:user][:username])
+    if @user.update(user_params)
       redirect_to user_path(@user)
     else
       flash[:error] = @user.errors.full_messages
